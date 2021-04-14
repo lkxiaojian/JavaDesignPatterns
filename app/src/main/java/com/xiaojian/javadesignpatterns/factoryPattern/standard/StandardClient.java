4b5ddee520b5aa08007906cc8e51b66048a39f27
lk <18513075404>
2021-04-14 14:45:54 +0800

    标准工厂模式

--- app/src/main/java/com/xiaojian/javadesignpatterns/factoryPattern/standard/StandardClient.java
+++ app/src/main/java/com/xiaojian/javadesignpatterns/factoryPattern/standard/StandardClient.java
@@ -3,7 +3,7 @@ package com.xiaojian.javadesignpatterns.factoryPattern.standard;
 /**
  * create_time : 21-4-13 下午3:19
  * author: lk
- * description： SimpleClient 使用方法
+ * description： SimpleClient 标准的工厂模式
  */
 public class StandardClient {
 
